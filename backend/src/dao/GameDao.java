70e7f0f0f9c15ef378b96a70e770bfbeb4535377
Arno Bidet <arno.bidet479@gmail.com>
2024-06-02 17:15:24 +0200

    refactor: Remove unused imports and clean up GameDao and PlayerDao

--- backend/src/dao/GameDao.java
+++ backend/src/dao/GameDao.java
@@ -4,30 +4,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import database.PolyNameDatabase;
 import model.Game;
 
 public class GameDao extends GenericDao {
-
-	protected database.MySQLDatabase database;
-
 	public GameDao() {
 		super();
 	}
 
 	public void createGame(String id) throws SQLException {
-
-		this.database = new PolyNameDatabase();
-
 		PreparedStatement statement = this.database.prepareStatement("INSERT INTO Game (game_code) VALUES (?)");
 		statement.setString(1, id);
-
 	}
 
 	private Game generateGameDaoFromResultSet(ResultSet results) throws SQLException {
 		final String game_code = results.getString("game_code");
-
 		return new Game(game_code);
-
 	}
 }
--- backend/src/dao/PlayerDao.java
+++ backend/src/dao/PlayerDao.java
@@ -1,11 +1,23 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.Player;
 
-public class PlayerDao {
+public class PlayerDao extends GenericDao{
+    public PlayerDao() {
+        super();
+    }
+
+    public void createPlayer(String cookie, String game_code) throws SQLException {
+        PreparedStatement statement = this.database.prepareStatement("INSERT INTO Player (cookie, host, game_code) VALUES (?, ?, ?, ?)");
+        statement.setString(1, cookie);
+        statement.setBoolean(2, true);
+        statement.setString(3, game_code);
+        statement.executeUpdate();
+    }
 
     private Player generatePlayerFromResultSet(ResultSet results) throws SQLException {
         final String cookie = results.getString("cookie");
