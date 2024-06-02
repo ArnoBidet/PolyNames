91873e9a5182ad1728b5d6d572409c82525d2fe7
Arno Bidet <arno.bidet479@gmail.com>
2024-06-02 16:51:02 +0200

    feat: Refactor PlayerDao to improve code readability and maintainability

--- backend/src/dao/PlayerDao.java
+++ backend/src/dao/PlayerDao.java
@@ -6,13 +6,12 @@ import java.sql.SQLException;
 import model.Player;
 
 public class PlayerDao {
-    
+
     private Player generatePlayerFromResultSet(ResultSet results) throws SQLException {
         final String cookie = results.getString("cookie");
         final boolean host = results.getBoolean("host");
         final String player_role = results.getString("player_role");
         final String game_code = results.getString("game_code");
-        return new Player(cookie,host,player_role,game_code);
+        return new Player(cookie, host, player_role, game_code);
     }
 }
-
