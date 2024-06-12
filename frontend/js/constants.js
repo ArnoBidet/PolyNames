18c0b729e627c3fac350599795fcd25fbf7b7635
ArnoBidet <arno.bidet479@gmail.com>
2024-06-12 20:03:59 +0200

    refactor: Update MIN_WIDTH constant in constants.js to 600

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "http://localhost:8081/";
 export const SSE_URL = "localhost:8081"
-export const MIN_WIDTH = 800;
+export const MIN_WIDTH = 600;
 export const MIN_HEIGHT = 400;
